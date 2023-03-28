e9e8e2af3e376e2cf7d577f8f872a27c1fc44ce5
Paul Markoff <paulmarkoff@pauls-mbp.lan>
2023-03-28 11:33:24 -0700

    added cart icon and dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,8 @@ import { signOut } from "firebase/auth";
 import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg'
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import './navigation.styles.scss';
@@ -35,7 +37,9 @@ const Navigation = () => {
                 )
               }
             </Link>
+            <CartIcon />
           </div>
+          <CartDropdown />
         </div>
         <Outlet />
       </Fragment>
